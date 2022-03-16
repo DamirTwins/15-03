db2d30ecb80216086605749353835ba45944c3c9
damiralymbekuulu <damir.alymbek@gmail.com>
2022-03-16 11:46:42 +0600

    feat: added new content

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Content from "./Content";
+import Footer from "./Footer";
+import Header from "./Header";
+
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Content/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App;
